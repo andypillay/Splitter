be0634c5ea7d3e9b4179d770b93a39a7ffd900e4
Andy Pillay <andypillay93@gmail.com>
2018-11-10 18:25:07 +0000

    Created sender balance splitter.

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -2,6 +2,39 @@ pragma solidity ^0.4.22;
 
 
 contract Splitter {
-  constructor() {
+  struct Account {
+    bool isMember;
+    uint balance;  
+  } 
+
+  mapping (address=>Account) private accounts;
+  address[] private addressIndices;
+
+  event ContactInit(address[] addresses);
+
+  modifier paymentSenderIsAuth() {
+    require(accounts[msg.sender].isMember, "Account is not authourised member.");
+    _;
   }
+
+  //INIT CONTRACT
+  constructor (address[] addresses) public {
+    require(addresses.length == 3);
+    {
+      for (uint i = 0; i < addresses.length; i++) {
+        accounts[addresses[i]] = Account(true,0);
+        addressIndices.push(addresses[i]);
+    }
+    emit ContactInit(addresses);
+    }
+  }
+
+  function withdraw(uint reqAmount, bool all) internal senderIsUser returns(bool isSuccess){
+
+  }
+
+
+
+
+
 }
