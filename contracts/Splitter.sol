b1889ed0b9bb048203cfb2a6e8d5f93ec77204f5
Andy Pillay <andypillay93@gmail.com>
2018-11-11 15:03:44 +0000

    getOtherAccount changes and made split balance handle an number for users.

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -4,11 +4,13 @@ pragma solidity ^0.4.22;
 contract Splitter {
   struct Account {
     bool isMember;
-    uint balance;  
+    uint balance;
+    bool isSender;
   } 
 
   mapping (address=>Account) private accounts;
-  address[] private addressIndices;
+  address[] private addrs;
+  address[] nonSenderaddresses;
 
   uint public currentBalance;
 
@@ -25,35 +27,38 @@ contract Splitter {
     require(addresses.length == 3);
     {
       for (uint i = 0; i < addresses.length; i++) {
-        accounts[addresses[i]] = Account(true,0);
-        addressIndices.push(addresses[i]);
+        accounts[addresses[i]] = Account(true,0,false);
+        addrs.push(addresses[i]);
     }
     emit ContactInit(addresses);
     }
   }
 
-  function getOtherAccounts() internal view paymentSenderIsAuth returns (address[] otherUsers) {
-    if (addressIndices[0] == msg.sender) {
-      otherUsers.push(addressIndices[1]);
-      otherUsers.push(addressIndices[2]);
-      return(otherUsers);
-    } 
+  function getOtherAccounts() internal paymentSenderIsAuth returns (address[] recievers) {
+    for (uint i = 0; i < addrs.length; i++) {
+      if (addrs[i] == msg.sender) {
+        accounts[msg.sender].isSender = true;
+      } else {
+        accounts[addrs[i]].isSender = false;
+        nonSenderaddresses.push(addrs[i]);
+      }
+    }
+    return nonSenderaddresses;
   }
 
   function splitBalance() public paymentSenderIsAuth returns (bool success) {
-    (bob,carol) = getOtherAccounts();
     currentBalance = accounts[msg.sender].balance;
     accounts[msg.sender].balance = 0;
+    address[] memory recievers;
+    recievers = getOtherAccounts();
+    uint numberOfRecivers = recievers.length;
+    uint cut = currentBalance/numberOfRecivers;
 
-    accounts[bob].balance += currentBalance/2;
-    accounts[carol].balance += currentBalance/2;
+    for (uint index = 0; index <= numberOfRecivers; index++) {
+        accounts[recievers[index]].balance += cut;
+    }
 
     emit BalanceSplit(msg.sender);
     return true;
   }
-
-
-
-
-
 }
