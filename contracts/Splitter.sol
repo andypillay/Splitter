8fd56ac75a8ab608a910a0f8c9f2141f412aef7a
andypillay <andypillay93@gmail.com>
2018-11-21 17:00:01 +0000

    Third PR Changes. WIP

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -1,47 +1,54 @@
-pragma solidity ^0.4.22;
+pragma solidity 0.4.24;
 
 contract Splitter {
 
-  address owner;
+  address public owner;
 
   mapping (address => uint) public balances;
 
   event LogContractInit(address sender);
-  event LogFundsRecieved(address sender ,address reciever1, address reciever2, uint amountRecieved);
+  event LogFundsRecieved(address indexed sender ,address indexed reciever1, address indexed reciever2, uint amountRecieved);
   event LogWithdraw(address sender, uint balance);
-  //INIT CONTRACT
-  constructor () public {
-      owner = msg.sender;
-      emit LogContractInit(msg.sender);
-    }
-
-  function sendSplit(address reciever1, address reciever2) public payable onlyOwner returns(bool success) {
-      require(msg.value > 0);
-      require(reciever1 != 0);
-      require(reciever2 != 0);
-      require(msg.value % 2 == 0);
-      uint value = msg.value/2;
-      balances[reciever1] += value;
-      balances[reciever2] += value;
-      emit LogFundsRecieved(msg.sender,reciever1,reciever2,value);
-      return true;
-    }
-
-  function withdraw()public onlyOwner returns(bool success) {
-    require(balances[msg.sender] > 0);
-    uint amount = balances[msg.sender];
-    balances[msg.sender] = 0;
-    emit LogWithdraw(msg.sender, amount);
-    msg.sender.transfer(amount);
-    return true;
-  }
 
   modifier onlyOwner {
     require(owner == msg.sender);
     _;
   }
 
-  function checkBalance(address user)public constant returns(uint balance) {
-    return balances[user];
+
+  constructor () public {
+      owner = msg.sender;
+      emit LogContractInit(msg.sender);
+    }
+
+  function sendSplit(address reciever1, address reciever2) public payable returns(bool success) {
+      require(msg.value > 0);
+      require(reciever1 != 0);
+      require(reciever2 != 0);
+      if(msg.value % 2 == 0){
+        uint value = msg.value/2;
+        balances[reciever1] += value;
+        balances[reciever2] += value;
+        emit LogFundsRecieved(msg.sender,reciever1,reciever2,value);
+        return true;
+      }
+      else
+      {
+        uint valueBonus = (msg.value + 1)/2;
+        balances[reciever1] += valueBonus;
+        balances[reciever2] += valueBonus;
+        emit LogFundsRecieved(msg.sender,reciever1,reciever2,value);
+        return true;
+      }
+    }
+
+  function withdraw()public onlyOwner returns(bool success) {
+    uint amount = balances[msg.sender];
+    require(balances[msg.sender] > 0);
+    balances[msg.sender] = 0;
+    emit LogWithdraw(msg.sender, amount);
+    msg.sender.transfer(amount);
+    return true;
   }
+
 }
--- test/test_splitter.js
+++ test/test_splitter.js
@@ -1,5 +1,5 @@
 const Splitter = artifacts.require("./Splitter.sol");
-Promise = require("bluebird");
+const Promise = require("bluebird");
 Promise.promisifyAll(web3.eth, { suffix: "Promise" });
 
 contract('Splitter', accounts => {
@@ -13,41 +13,40 @@ contract('Splitter', accounts => {
   beforeEach('setup', async () => {contract = await Splitter.new({from: alice})});
   
   it('should be owned by alice', function() {
-    return contract({from: alice})
+    return contract.owner({from: alice})
       .then(function(owner) {
         assert.strictEqual(owner, alice);
       });
   });
 
-  
   it('should split even amounts', async (isSuccessful) => {
     return await contract.sendSplit(bob, carol, {from: alice, value: 100 })
-      .then((tx) => {
-         contract.checkBalance(alice).then((balance) => {
-          assert.equal('0', balance);
-        });
+      .then(tx => assert.equal('0', tx.contract.balances[alice]))
+      .then(tx => contract.balances[bob])
+      .then(balance => assert.equal('50', balance.toString(10)))
+      .then(tx => contract.balances[carol])
+      .then(balance => assert.equal('50', balance.toString(10)))
+  });
 
-         contract.checkBalance(bob).then((balance) => {
-          assert.equal('50', balance);
-        });
-
-         contract.checkBalance(carol).then((balance) => {
-          assert.equal('50', balance);
-        });
-      });
-    }
-  );
+  it('should split odd amounts', async (isSuccessful) => {
+    return await contract.sendSplit(bob, carol, {from: alice, value: 101 })
+      .then(tx => contract.balances[alice])
+      .then(balance => assert.equal('0', balance.toString(10)))
+      .then(tx => contract.balances[bob])
+      .then(balance => assert.equal('50', balance.toString(10)))
+      .then(tx => contract.balances[carol])
+      .then(balance => assert.equal('50', balance.toString(10)));
+  });
 
   it('should be able to withdraw', async (isSuccessful) => {
-    var oldBalance = web3.eth.getBalance(bob);
+    var oldBalance = web3.eth.getBalancePromise(bob);
     var price = 0;
-    return await contract.sendSplit(bob, carol, {from: alice, value: 100, gasPrice: 1})
-      .then(tx => {
-        return contract.withdraw({from: bob, gasPrice: 1})
-      })
+    return await contract.sendSplit(bob, carol, {from: alice, value: 100})
+      .then(tx => contract.withdraw({from: bob}))
       .then(tx => {
         price = tx.receipt.gasUsed;
-        assert.equal(oldBalance.minus(price).plus(50).toString(), web3.eth.getBalance(bob));
+        assert.equal(oldBalance.minus(price).plus(50).toString(), web3.eth.getBalancePromise(bob));
       });
   });
+
 });
